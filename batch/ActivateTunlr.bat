6bd985a0861bd0165bd1fd1eb73fd2dd9d5e0a8b
Kartik Talwar <hi@kartikt.com>
2012-08-06 14:24:19 -0400

    :tv: and :musical_note: in `MURICA :us:

--- batch/ActivateTunlr.bat
+++ batch/ActivateTunlr.bat
@@ -2,4 +2,6 @@
 :: Browse sites in 'Murica
 netsh interface ipv4 add dnsservers name="Local Area Connection" 149.154.158.186
 netsh interface ipv4 add dnsservers name="Local Area Connection" 199.167.30.144 index=2
+netsh interface ipv4 add dnsservers name="Wireless Network Connection" 149.154.158.186
+netsh interface ipv4 add dnsservers name="Wireless Network Connection" 199.167.30.144 index=2
 ipconfig /flushdns
--- batch/DeactivateTunlr.bat
+++ batch/DeactivateTunlr.bat
@@ -1,4 +1,5 @@
 :: Deactivate Tunlr DNS
 :: Switch to local settings
 netsh interface ipv4 set dnsservers name="Local Area Connection" source=dhcp 
+netsh interface ipv4 set dnsservers name="Wireless Network Connection" source=dhcp 
 ipconfig /flushdns
