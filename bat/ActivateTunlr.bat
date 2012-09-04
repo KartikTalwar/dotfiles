d9a6d429519ade8e4730fd1dfd7c79086871ced7
Kartik <hi@kartikt.com>
2012-09-04 18:55:33 -0300

    Updated DNS names to watch :tv: and :musical_note: in `MURICA :us:

--- bat/ActivateTunlr.bat
+++ bat/ActivateTunlr.bat
@@ -1,7 +1,7 @@
 :: Activate Tunlr DNS
 :: Browse sites in 'Murica
-netsh interface ipv4 add dnsservers name="Local Area Connection" 149.154.158.186
+netsh interface ipv4 add dnsservers name="Local Area Connection" 184.82.222.5
 netsh interface ipv4 add dnsservers name="Local Area Connection" 199.167.30.144 index=2
-netsh interface ipv4 add dnsservers name="Wireless Network Connection" 149.154.158.186
+netsh interface ipv4 add dnsservers name="Wireless Network Connection" 184.82.222.5
 netsh interface ipv4 add dnsservers name="Wireless Network Connection" 199.167.30.144 index=2
 ipconfig /flushdns
